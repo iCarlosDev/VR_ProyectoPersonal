8b655639dac33b1fe75dd0f7a8b79c3e73b30ea1
lmYourGod <73672853+lmYourGod@users.noreply.github.com>
2023-04-17 21:01:45 +0200

    [Grab Objects] + [Enemy NavMesh]

--- VR_ProyectoPersonal/Assets/Hall/Scripts/Enemy/EnemyController.cs
+++ VR_ProyectoPersonal/Assets/Hall/Scripts/Enemy/EnemyController.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] private Animator _animator;
+    [SerializeField] private NavMeshAgent _navMeshAgent;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _navMeshAgent.SetDestination(GameObject.FindWithTag("Player").transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        AnimationControl();
+    }
+
+    private void AnimationControl()
+    {
+        _animator.SetFloat("NavMeshVelocity", _navMeshAgent.velocity.magnitude);
     }
 }
